79a11f139d8fdd636fcde7513ed88dbd09f3001c
Sheldon O'Reilly <sheldon.oreilly@gmail.com>
2018-08-29 18:56:08 -0230

    Added tests for delete endpoint

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -110,4 +110,45 @@ describe("GET /todos/:id", () => {
 	});
 });
 
-//mongo: mongodb://<dbuser>:<dbpassword>@ds233228.mlab.com:33228/todoapi
+describe("DELETE /todos/:id", () => {
+	/*it("should remove a todo", done => {
+		var hexId = todos[1]._id.toHexString();
+
+		console.log("hexId :", hexId);
+
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(200)
+			.expect(res => {
+				expect(res.body.todo._id).toBe(hexId);
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+
+				Todo.findById(hexId)
+					.then(todo => {
+						expect(todo).toNotExist();
+						done();
+					})
+					.catch(e => done(e));
+			});
+	});*/
+
+	it("should return 404 if todo not found", done => {
+		var hexId = new ObjectID().toHexString();
+
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(404)
+			.end(done);
+	});
+
+	it("should return 404 if object id is invalid", done => {
+		request(app)
+			.delete("/todos/123abc")
+			.expect(404)
+			.end(done);
+	});
+});
