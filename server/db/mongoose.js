5c41d657ac8da875b65fd7a8c169929fb13c80ab
Sheldon O'Reilly <sheldon.oreilly@gmail.com>
2018-09-03 23:21:02 -0230

    Change from local mongo

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 //local
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+//mongoose.connect("mongodb://host:27017/TodoApp");
 //heroku
-//mongoose.connect("mongodb://SheldonO:Dolphins01_@ds233228.mlab.com:33228/todoapi");
+mongoose.connect("mongodb://SheldonO:Dolphins01_@ds233228.mlab.com:33228/todoapi");
 
 module.exports = { mongoose };
